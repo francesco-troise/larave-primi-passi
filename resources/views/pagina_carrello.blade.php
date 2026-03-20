f05ea16e15930809edbbfb5abdc1924a21c2067d
francesco-troise <francesco.troise98@gmail.com>
2026-03-20 12:50:32 +0100

    Added views, added header with link to the views, added routes for the views

---
+++ resources/views/pagina_carrello.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Carrello</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li><a href="./pagina_profilo">Pagina profilo</a></li>
+                <li><a href="./pagina_carrello">Carrello</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
